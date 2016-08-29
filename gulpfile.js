09a39e877b13281b1f05a14318b98f0b9611b2e7
tag9724 <gaetanburg@gmail.com>
2016-08-29 17:21:01 +0200

    Gulp config

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var gulp_sync_task = require('gulp-sync-task');
+
+var src = 'src';
+var bin = 'bin';
+
+gulp.task('clean', function() {
+    console.log("[START] Clear bin folder ...");
+
+    return gulp.src(bin + '/**/*.*', {
+            read: false
+        })
+        .pipe(plugins.clean());
+});
+
+gulp.task('copy', function() {
+    console.log("[COPY] cp ./src/ to ./bin/ ...");
+
+    return gulp.src(src + '/**/*.*')
+        .pipe(gulp.dest('bin'));
+});
+
+gulp.task('html', function() {
+    console.log("[HTML] minify ...");
+
+    return gulp.src(bin + '/**/*.html')
+        .pipe(plugins.htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('bin'));
+});
+
+gulp.task('css', function() {
+    console.log("[CSS] minify ...");
+
+    return gulp.src(bin + '/**/*.css')
+        .pipe(plugins.csso())
+        .pipe(gulp.dest(bin));
+});
+
+gulp.task('js', function() {
+    console.log("[JS] minify ...");
+
+    gulp.src(bin + '/**/*.js')
+        .pipe(plugins.jsmin())
+        .pipe(gulp.dest(bin));
+});
+
+gulp.task('minify', ['html', 'css', 'js']);
+gulp.task('default', gulp_sync_task('clean', 'copy', 'minify'));
